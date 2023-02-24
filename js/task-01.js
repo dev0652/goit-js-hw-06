5a69037380b56da6089a2e3a81277a3f01b492df
audi0652 <113797010+dev0652@users.noreply.github.com>
2023-02-24 11:25:08 +0200

    corrected task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,21 @@
-const categoriesRef = document.querySelector("#categories");
+const categoriesRef = document.querySelector('#categories');
 
-console.log("Number of categories:", categoriesRef.children.length);
+console.log('Number of categories:', categoriesRef.children.length);
 
 const getCategories = (categories) => {
   return [...categories.children].forEach((category) => {
-    console.log("Category:", category.querySelector(".item h2").textContent);
     console.log(
-      "Elements:",
-      category.querySelector(".item ul").children.length
+      'Category:',
+      category.firstElementChild.textContent
+      // or, alternatively:
+      // category.querySelector('h2').textContent
+    );
+
+    console.log(
+      'Elements:',
+      category.lastElementChild.children.length
+      // or, alternatively:
+      // category.querySelector('ul').children.length
     );
   });
 };
