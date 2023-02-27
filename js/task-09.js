e60e7a4576fb09a9ce584000e6151e5c13c8df6d
audi0652 <113797010+dev0652@users.noreply.github.com>
2023-02-27 03:45:59 +0200

    9 done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorBtnEl = document.querySelector('.change-color');
+const printRgb = document.querySelector('.color');
+
+changeColorBtnEl.addEventListener('click', changeColor);
+
+function changeColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  printRgb.textContent = document.body.style.backgroundColor.toString();
+}
