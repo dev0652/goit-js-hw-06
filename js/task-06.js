ca801db227a80485dac965720732494f7985b236
audi0652 <113797010+dev0652@users.noreply.github.com>
2023-02-27 21:55:18 +0200

    tmp

--- js/task-06.js
+++ js/task-06.js
@@ -4,6 +4,7 @@ inputEl.addEventListener('blur', onInputBlur);
 
 function onInputBlur(event) {
   event.currentTarget.classList.remove('valid', 'invalid');
+
   event.currentTarget.classList.add(
     event.currentTarget.textLength == event.currentTarget.dataset.length
       ? 'valid'
