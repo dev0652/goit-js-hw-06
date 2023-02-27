07795ad29b9dd9a30550127c82136246eb4b0eee
audi0652 <113797010+dev0652@users.noreply.github.com>
2023-02-27 03:37:28 +0200

    8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return alert('All fields must be filled');
+  }
+
+  const credentials = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(credentials);
+  event.currentTarget.reset();
+}
