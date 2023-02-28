be5f68ddd36cfa0d60b2be5f7102fe9ac800f430
audi0652 <113797010+dev0652@users.noreply.github.com>
2023-02-28 22:50:23 +0200

    tmp

--- js/task-07.js
+++ js/task-07.js
@@ -1,8 +1,9 @@
 const sliderEl = document.getElementById('font-size-control');
 const spanEl = document.getElementById('text');
+spanEl.style.fontSize = sliderEl.value + 'px';
 
 sliderEl.addEventListener('input', updateFontSize);
 
 function updateFontSize(event) {
-  spanEl.style.fontSize = event.currentTarget.value + 'px';
+  spanEl.style.fontSize = event.target.value + 'px';
 }
